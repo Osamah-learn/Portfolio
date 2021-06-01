da1de6ca3e7c6672d13677e1ec46ed502080b983
Osamah-learn <77615959+Osamah-learn@users.noreply.github.com>
2021-06-01 13:39:01 +0300

    Bug fixed

--- main.js
+++ main.js
@@ -1,79 +1,64 @@
-
-
-
 /* Loader Animation */
 
 const loader = document.querySelector(".loader");
 const main = document.querySelector(".main");
 
-
-function init(){
-    setTimeout(()=>{
-loader.style.opacity=0;
-loader.style.display="none";
-console.log("done")
-main.style.display="block";
-setTimeout(()=>{
-    main.style.opacity=1,50;
-},2000)
-
-    }, 3000);
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+    console.log("done");
+    main.style.display = "block";
+    setTimeout(() => {
+      (main.style.opacity = 1), 50;
+    }, 2000);
+  }, 3000);
 }
-
 
 init();
 
 /* Loader Conaspt End*/
 
-
 /* Burger Animation  */
 
+// Look for .hamburger
 
- // Look for .hamburger
- 
- const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger");
 
-
- //looking for nav links
- const nav = document.querySelector(".nav-links");
- const links = document.querySelectorAll("a");
- window.onload = (event) => {
-  
+//looking for nav links
+const nav = document.querySelector(".nav-links");
+const links = document.querySelectorAll("a");
+window.onload = (event) => {
   nav.classList.toggle("nav-open");
 };
- // On click
- hamburger.addEventListener("click", function() {
-   // Toggle class "is-active"
-   
-    hamburger.classList.toggle("is-active");
-  
-  
-   
-   nav.classList.toggle("nav-open");
- })
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
 
- links.forEach(link=>{
-   link.addEventListener("click",()=>{
+  hamburger.classList.toggle("is-active");
+
+  nav.classList.toggle("nav-open");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
-   })
- })
-   // Do something else, like open/close menu
+  });
+});
+// Do something else, like open/close menu
 
 /* Burger Animation Ened */
-
-
-
-
 
 /* Typping Effects */
 
 var i = 0;
-var text = "Hi There ! My name is Osamah and I’m a front end developer from Finland. Welcome to my website...";
-function typing(){
-  if (i<text.length){
-    document.getElementById("text").innerHTML+=text.charAt(i);
+var text =
+  "Hi There ! My name is Osamah and I’m a front end developer from Finland. Welcome to my website...";
+function typing() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing,500)
+    setTimeout(typing, 500);
   }
 }
 
